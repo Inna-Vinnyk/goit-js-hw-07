6995ff9948fc6c3727e147420eeaf53f6247d1d4
Inna-Vinnyk <vinnik_inna@ukr.net>
2025-03-22 00:08:24 +0100

    add files .js and file common.css

--- js/task-3.js
+++ js/task-3.js
@@ -1,41 +1,11 @@
-class StringBuilder {
-  #value;
+//* Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім'я для привітання.
+//* Обов'язково очищай значення в інпуті по краях від пробілів .
+//* Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+const inputName = document.getElementById('name-input');
+const outputName = document.getElementById('name-output');
 
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
-
-//Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
-
-// Оголоси наступні методи класу:
-
-//getValue() — повертає поточне значення приватної властивості value.
-//padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
-//padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
-//padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+inputName.addEventListener('input', () => {
+  const uName = inputName.value.trim();
+  outputName.textContent = uName || 'Anonymous';
+});
